c2777b2b1febf738b4462a73493c3f8c1c7d3d1e
Konnor88 <39319697+Konnor88@users.noreply.github.com>
2025-12-07 00:29:46 -0500

    Add files via upload

---
+++ AgentpediaTableGenerator/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 41)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(294, 267)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Table"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(220, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Open Agent..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 311)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(306, 39)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Note if a character has both a 1.5 and 2.0 version you will need" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "to add the appr" &
+    "opriate {{Exclusive/1.5}} or {{Exclusive/2.0}}" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "template right beside the charac" &
+    "ter name!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(328, 360)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(344, 399)
+        Me.MinimumSize = New System.Drawing.Size(344, 399)
+        Me.Name = "Form1"
+        Me.Text = "Agentpedia Table Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ AgentpediaTableGenerator/Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System.IO
+Imports System.Text
+Imports AgentObjects
+
+Public Class Form1
+    Private MyAgent As AgentObjects.Agent
+    Private Character As AgentObjects.IAgentCtlCharacterEx
+
+    Private Shared ReadOnly TtsVoiceMap As New Dictionary(Of String, String)(StringComparer.OrdinalIgnoreCase) From {
+        {"{CA141FD0-AC7F-11D1-97A3-006008273008}", "Adult Female #1, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273009}", "Adult Female #2, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273000}", "Adult Male #1, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273001}", "Adult Male #2, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273002}", "Adult Male #3, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273003}", "Adult Male #4, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273004}", "Adult Male #5, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273005}", "Adult Male #6, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273006}", "Adult Male #7, American English (TruVoice)"},
+        {"{CA141FD0-AC7F-11D1-97A3-006008273007}", "Adult Male #8, American English (TruVoice)"},
+        {"{227A0E40-A92A-11D1-B17B-0020AFED142E}", "Carol, British English (L&H TTS3000)"},
+        {"{227A0E41-A92A-11D1-B17B-0020AFED142E}", "Peter, British English (L&H TTS3000)"},
+        {"{A0DDCA40-A92C-11D1-B17B-0020AFED142E}", "Linda, Dutch (L&H TTS3000)"},
+        {"{A0DDCA41-A92C-11D1-B17B-0020AFED142E}", "Alexander, Dutch (L&H TTS3000)"},
+        {"{0879A4E0-A92C-11D1-B17B-0020AFED142E}", "Véronique, French (L&H TTS3000)"},
+        {"{0879A4E1-A92C-11D1-B17B-0020AFED142E}", "Pierre, French (L&H TTS3000)"},
+        {"{3A1FB760-A92B-11D1-B17B-0020AFED142E}", "Anna, German (L&H TTS3000)"},
+        {"{3A1FB761-A92B-11D1-B17B-0020AFED142E}", "Stefan, German (L&H TTS3000)"},
+        {"{7EF71700-A92D-11D1-B17B-0020AFED142E}", "Barbara, Italian (L&H TTS3000)"},
+        {"{7EF71701-A92D-11D1-B17B-0020AFED142E}", "Stefano, Italian (L&H TTS3000)"},
+        {"{A778E060-A936-11D1-B17B-0020AFED142E}", "Naoko, Japanese (L&H TTS3000)"},
+        {"{A778E061-A936-11D1-B17B-0020AFED142E}", "Kenji, Japanese (L&H TTS3000)"},
+        {"{12E0B720-A936-11D1-B17B-0020AFED142E}", "Shin-Ah, Korean (L&H TTS3000)"},
+        {"{12E0B721-A936-11D1-B17B-0020AFED142E}", "Jun-Ho, Korean (L&H TTS3000)"},
+        {"{8AA08CA0-A1AE-11D3-9BC5-00A0C967A2D1}", "Juliana, Portuguese (Brazil) (L&H TTS3000)"},
+        {"{8AA08CA1-A1AE-11D3-9BC5-00A0C967A2D1}", "Alexandre, Portuguese (Brazil) (L&H TTS3000)"},
+        {"{06377F80-D48E-11D1-B17B-0020AFED142E}", "Svetlana, Russian (L&H TTS3000)"},
+        {"{06377F81-D48E-11D1-B17B-0020AFED142E}", "Boris, Russian (L&H TTS3000)"},
+        {"{2CE326E0-A935-11D1-B17B-0020AFED142E}", "Carmen, Spanish (L&H TTS3000)"},
+        {"{2CE326E1-A935-11D1-B17B-0020AFED142E}", "Julio, Spanish (L&H TTS3000)"}
+    }
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MyAgent = New AgentObjects.Agent()
+        MyAgent.Connected = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using ofd As New OpenFileDialog()
+            ofd.Title = "Select a Microsoft Agent character"
+            ofd.Filter = "Agent characters (*.acs;*.acf)|*.acs;*.acf|All files (*.*)|*.*"
+
+            If ofd.ShowDialog() <> DialogResult.OK Then
+                Return
+            End If
+
+            LoadCharacterAndFillTable(ofd.FileName)
+        End Using
+    End Sub
+
+    Private Sub LoadCharacterAndFillTable(filePath As String)
+        Try
+            If Character IsNot Nothing Then
+                Try
+                    MyAgent.Characters.Unload("Char")
+                Catch
+                End Try
+                Character = Nothing
+            End If
+
+            Dim req = MyAgent.Characters.Load("Char", filePath)
+            Application.DoEvents()
+
+            Character = CType(MyAgent.Characters("Char"), AgentObjects.IAgentCtlCharacterEx)
+
+            Dim fi As New FileInfo(filePath)
+            Dim sizeMB As Double = fi.Length / (1024.0 * 1024.0)
+            Dim sizeString As String = sizeMB.ToString("0.00") & " MB"
+
+            Dim animCount As Integer = 0
+            For Each anim As Object In Character.AnimationNames
+                animCount += 1
+            Next
+
+            Dim widthPx As Integer = CInt(Character.OriginalWidth)
+            Dim heightPx As Integer = CInt(Character.OriginalHeight)
+
+            Dim ttsModeId As String = Character.TTSModeID
+            Dim ttsVoice As String = ""
+
+            If Not String.IsNullOrWhiteSpace(ttsModeId) Then
+                Dim mapped As String = Nothing
+                If TtsVoiceMap.TryGetValue(ttsModeId, mapped) Then
+                    ttsVoice = mapped
+                End If
+            End If
+
+            Dim sb As New StringBuilder()
+
+            sb.AppendLine("{{Character data table")
+            sb.AppendLine("| character name = " & Character.Name)
+            sb.AppendLine("| file name = " & Path.GetFileName(filePath))
+            sb.AppendLine("| description = " & Character.Description)
+            sb.AppendLine("| extra data = " & Character.ExtraData)
+            sb.AppendLine("| file size = " & sizeString)
+            sb.AppendLine("| animations = " & animCount.ToString())
+            sb.AppendLine("| tts voice = " & ttsVoice)
+            sb.AppendLine("| guid = " & Character.GUID)
+            sb.AppendLine("| ttsmode id = " & ttsModeId)
+            sb.AppendLine("| tts speed = " & Character.Speed.ToString())
+            sb.AppendLine("| tts pitch = " & Character.Pitch.ToString())
+            sb.AppendLine("| width = " & widthPx.ToString() & "px")
+            sb.AppendLine("| length = " & heightPx.ToString() & "px")
+            sb.AppendLine("}}")
+
+            RichTextBox1.Text = sb.ToString()
+
+        Catch ex As Exception
+            MessageBox.Show("There appears to be an error with the character you loaded: " & ex.Message,
+                            "Agentpedia Table Creator",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+End Class
